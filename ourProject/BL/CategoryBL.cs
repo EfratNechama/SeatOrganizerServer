a5f4a7db13f2dffa5fa4635cc823fdb7810abf21
Nechama <neproject2@gmail.com>
2022-01-27 00:21:23 +0200

    note: eventPerUser doesn't work

--- ourProject/BL/CategoryBL.cs
+++ ourProject/BL/CategoryBL.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BL//Hello for try git!
+namespace BL//Hello for try git!//
 {
    public class CategoryBL:ICategoryBL
     {
